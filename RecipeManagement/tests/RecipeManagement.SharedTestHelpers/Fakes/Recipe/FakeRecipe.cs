7bedc89d02365f29c25031b3b61d5d961e6fb977
Paul DeVito <pdevito3@gmail.com>
2022-01-29 18:29:18 -0500

    Initial Commit

---
+++ RecipeManagement/tests/RecipeManagement.SharedTestHelpers/Fakes/Recipe/FakeRecipe.cs
@@ -0,0 +1,13 @@
+namespace RecipeManagement.SharedTestHelpers.Fakes.Recipe;
+
+using AutoBogus;
+using RecipeManagement.Domain.Recipes;
+using SharedKernel.Dtos.RecipeManagement.Recipe;
+
+public class FakeRecipe
+{
+    public static Recipe Generate(RecipeForCreationDto recipeForCreationDto)
+    {
+        return Recipe.Create(recipeForCreationDto);
+    }
+}
